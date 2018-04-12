1f5bddb428aebfd16d8600007e5916e35c34aca7
Tj Liggett <liggetttj@gmail.com>
2018-04-12 09:44:21 -0500

    Heroku test

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="{{ URL::asset('foundation/css/foundation.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('foundation/css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/foundation.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/override.css') }}">
     </head>
     <body>
         <div class="top-bar">
